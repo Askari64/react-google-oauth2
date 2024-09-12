a5155486398bfe25fa5e88bbb4efcf276cf453a9
Askari <askarir64@gmail.com>
2024-09-12 16:44:02 +0530

    Home Page Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,19 @@
 import useUserStore from "../store/userStore";
 import Profile from "../components/Profile";
 import LogoutButton from "../components/LogoutButton";
+import Card from "../components/Card";
 
 function Home() {
   const { user } = useUserStore();
   return (
-    <>
-      <Profile name={user.name} email={user.email} picture={user.picture} />
-      <LogoutButton />
-    </>
-  );
+    <div className=" w-dvw flex flex-col">
+      <div className=" flex flex-col gap-5 my-10 justify-center items-center">
+        <Profile name={user.name} email={user.email} picture={user.picture} />
+        <LogoutButton />
+      </div>
+      <Card />
+    </div>
+  );   
 }
 
 export default Home;
