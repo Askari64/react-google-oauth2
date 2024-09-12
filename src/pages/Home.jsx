a4b5c7454f346f8aea8f636e8f1ae718b7004c4b
Askari <askarir64@gmail.com>
2024-09-12 14:48:28 +0530

    Displays profile n logout button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import useUserStore from "../store/userStore";
 import Profile from "../components/Profile";
+import LogoutButton from "../components/LogoutButton";
 
 function Home() {
   const { user } = useUserStore();
-  return <Profile name={user.name} email={user.email} picture={user.picture} />;
+  return (
+    <>
+      <Profile name={user.name} email={user.email} picture={user.picture} />
+      <LogoutButton />
+    </>
+  );
 }
 
 export default Home;
